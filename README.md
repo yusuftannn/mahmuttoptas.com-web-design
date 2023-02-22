45eb42cb247b2f2f3cde81aea0cb4224714ab92a
Yusuf Tan <yusuftan41@hotmail.com>
2023-02-22 20:20:20 +0300

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+# gdscProject
+GDSC Düzce Web Site Tasarımı
+
+<h1>GDSC DUZCE<h1>
+#GDSC DUZCE Topluluğu Tüm Haklı Saklıdır
---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+
+ new Swiper('.etkinlik-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 120
+      }
+    }
+  });
